027a9df7053c8f8dc565ea00d5643045d1912ee3
Catchet <adam.agren@gmail.com>
2023-11-21 04:45:37 +0900

    Initial commit

--- content.js
+++ content.js
@@ -1,36 +1,43 @@
 // Function to calculate and update the total tally
 function updateTotal() {
-    const elements = document.querySelectorAll('.class1 a');
-    let sum = 0;
-  
-    elements.forEach(element => {
-      const number = parseFloat(element.textContent);
-      if (!isNaN(number)) {
-        sum += number;
-      }
-    });
-  
-    // Find or create the total element
-    let totalElement = document.getElementById('totalElement');
-    if (!totalElement) {
-      totalElement = document.createElement('a');
-      totalElement.id = 'totalElement';
-      document.body.appendChild(totalElement);
+  const elements = document.querySelectorAll('.puzzle__session a');
+  let sum = 0;
+
+  elements.forEach(element => {
+    const number = parseFloat(element.textContent);
+    if (!isNaN(number)) {
+      sum += number;
     }
-  
-    // Update the total element with the new sum
-    totalElement.textContent = `Total: ${sum}`;
+  });
+
+  // Find or create the total element
+  let totalElement = document.getElementById('totalElement');
+  if (!totalElement) {
+    totalElement = document.createElement('a');
+    totalElement.id = 'totalElement';
+    //document.body.appendChild(totalElement);
+    document.getElementsByClassName('puzzle__session')[0].prepend(totalElement);
   }
+
+  // Change colour depending on the score
+  if (sum >= 0)
+    totalElement.className = 'result-true';
+  else
+    totalElement.className = 'result-false';
+
+  // Update the total element with the new sum
+  totalElement.textContent = `Total: ${sum}`;
+}
   
-  // Initial calculation and display
-  updateTotal();
-  
-  // Use MutationObserver to watch for changes in the DOM
-  const observer = new MutationObserver(updateTotal);
-  
-  // Specify the target node and the type of mutations to observe
-  const targetNode = document.querySelector('.class1');
-  const config = { childList: true, subtree: true };
-  
-  // Start observing the target node for configured mutations
-  observer.observe(targetNode, config);
+// Initial calculation and display
+updateTotal();
+
+// Use MutationObserver to watch for changes in the DOM
+const observer = new MutationObserver(updateTotal);
+
+// Specify the target node and the type of mutations to observe
+const targetNode = document.querySelector('.puzzle__session');
+const config = { childList: true, subtree: true };
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
